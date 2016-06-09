aef1f632446a73d67024556940cf9d2e4562437b
angelvenchev <angelvenchev@yahoo.com>
2016-06-10 02:12:57 +0300

    Remove areas

--- Monument.Server/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Monument.Server.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
