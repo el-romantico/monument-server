fb1de0fe9bb262c12053fe69b0734cf721010346
angelvenchev <angelvenchev@yahoo.com>
2016-06-10 04:57:54 +0300

    Add english content

--- Monument.Service/StaticData/StaticData.cs
+++ Monument.Service/StaticData/StaticData.cs
@@ -37,7 +37,29 @@
         public static string NevskiEN {
             get
             {
-                return "nevski en";
+                return @"
+<h1>
+    Alexander Nevsky Cathedral
+</h1>
+<p>
+    <img src=""/Content/Images/1.jpg"" style=""width:100%;"" />
+</p>
+<p>
+    The St. Alexander Nevsky Cathedral (Bulgarian: Храм-паметник „Свети Александър Невски“, Hram-pametnik „Sveti Aleksandar Nevski“) is a Bulgarian Orthodox cathedral in Sofia, the capital of Bulgaria. Built in Neo-Byzantine style, it serves as the cathedral church of the Patriarch of Bulgaria and it is one of the largest Eastern Orthodox cathedrals in the world, as well as one of Sofia's symbols and primary tourist attractions. The St. Alexander Nevsky Cathedral in Sofia occupies an area of 3,170 square metres (34,100 sq ft) and can hold 10,000 people inside. It is the second-largest cathedral located on the Balkan Peninsula, after the Cathedral of Saint Sava in Belgrade.
+ </p>
+ <p>
+     The Alexander Nevsky Cathedral is a cross-domed basilica featuring an emphasized central dome. The cathedral's gold-plated dome is 45 m high (148 ft), with the bell tower reaching 53 metres (174 ft). The temple has 12 bells with total weight of 23 tons, the heaviest weighing 12 tons and the lightest 10 kilograms (22 lb). The interior is decorated with Italian marble in various colours, Brazilian onyx, alabaster, and other luxurious materials. The central dome has the Lord's Prayer inscribed around it, with thin gold letters.
+ </p>
+ <p>
+     The construction of the St. Alexander Nevsky Cathedral started in 1882 (having been planned since 19 February, 1879), when the foundation stone was laid, but most of it was built between 1904 and 1912. Saint Alexander Nevsky was a Russian prince. The cathedral was created in honour to the Russian soldiers who died during the Russo-Turkish War of 1877-1878, as a result of which Bulgaria was liberated from Ottoman rule.
+</p>
+<p>
+    The cathedral was designed by Alexander Pomerantsev, aided by Alexander Smirnov and Alexander Yakovlev, as the initial 1884-1885 project of Ivan Bogomolov was radically changed by Pomerantsev. The final design was finished in 1898, and the construction and decoration were done by a team of Bulgarian, Russian, Austro-Hungarian and other European artists, architects and workers, including the aforementioned architects, as well as Petko Momchilov, Yordan Milanov, Haralampi Tachev, Ivan Mrkvička, Vasily Bolotnov, Nikolay Bruni, Alexander Kiselyov, Anton Mitov and many others.
+</p>
+<p>
+    The marble parts and the lighting fixtures were created in Munich, the metal elements for the gates in Berlin, while the gates themselves were manufactured in Karl Bamberg's factory in Vienna, and the mosaics were shipped from Venice.
+</p>
+";
             }
         }
     }
