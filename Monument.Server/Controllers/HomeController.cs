080c8ec0af2745a6069c42f8b0f047001a4938ef
angelvenchev <angelvenchev@yahoo.com>
2016-06-10 04:30:18 +0300

    Add localization

--- Monument.Server/Controllers/HomeController.cs
+++ Monument.Server/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Monument.Server.Models;
 using Monument.Service;
+using System.IO;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace Monument.Server.Controllers
@@ -15,6 +17,11 @@ namespace Monument.Server.Controllers
 
         public ActionResult Data(int id, string locale)
         {
+            ViewBag.Locale = locale.ToLower() == "bg" ? "EN" : "BG";
+            ViewBag.Id = id;
+            //var baseUrl = Path.Combine(Request.Url.Scheme, Request.Url.Host);
+            //var route = Path.Combine(Request.Url.Segments.Take(4).ToList().Union(new[] { locale }).ToArray());
+            //ViewBag.LocalizationUrl = Path.Combine(baseUrl.ToString(), route.ToString());
             var data = DataService.GetData(id, locale);
             return View(model:data);
         }
